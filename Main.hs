2543e8a3523c2f35ae643d8939ea42e35aeb12c0
keizo <keizo.bookman@gmail.com>
2015-02-26 00:36:44 +0900

    WORK

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 import System.Environment
+import System.IO
 import Control.Monad.State
 import Control.Applicative
 import Data.Char (chr, ord, isNumber)
@@ -10,13 +11,13 @@ data BrainFuck = PtrInc | PtrDec | ValInc | ValDec | Put | Get | Loop [BrainFuck
     deriving (Show)
 
 ctx :: String
-ctx = " ========Compile Time execution=====\n\n\n"
+ctx = " ========execution=====\n\n\n"
 main :: IO ()
 main = do
     args <- getArgs 
     case args of
         []  -> return ()
-        [x] ->  ( readFile x ) >>= readProcessing >>= (\ src -> run $ execState(parse src)  [] ) >> return ()
+        [x] ->  ( readFile x ) >>= readProcessing >>= (\ src -> run $ reverse $ execState (parse src)  [] ) >> putChar '\n'
             where readProcessing src = putStrLn src >>  putStrLn ctx >> return src
         _ -> return ()
 
@@ -27,24 +28,20 @@ main = do
 parse :: String -> State  [BrainFuck] String
 parse  [] = return []
 parse src =  case src of
-    ('>':xs) -> get >>= (\s -> put (PtrInc:s) ) >> parse xs
-    ('<':xs) -> get >>= (\s -> put (PtrDec:s) ) >> parse xs
     ('+':xs) -> get >>= (\s -> put (ValInc:s) ) >> (parse xs)
     ('-':xs) -> get >>= (\s -> put (ValDec:s) )  >> parse xs
-    (',':xs) -> get >>= (\s -> put (Put:s) ) >> parse xs
-    ('.':xs) -> get >>= (\s -> put (Get:s) ) >> parse xs
-    ('[':xs) -> let (ys,y) = runState  (parse xs) []
-                in get >>= (\s -> put ((Loop  y):s) ) >> parse ys
+    ('>':xs) -> get >>= (\s -> put (PtrInc:s) ) >> parse xs
+    ('<':xs) -> get >>= (\s -> put (PtrDec:s) ) >> parse xs
+    ('.':xs) -> get >>= (\s -> put (Put:s) ) >> parse xs
+    (',':xs) -> get >>= (\s -> put (Get:s) ) >> parse xs
+    ('[':xs) -> let (y,ys) = runState  (parse xs) []
+                in get >>= (\s -> put ((Loop  ys):s) ) >> parse y
     (']':xs) ->  return []
-    _:xs -> parse xs
+    ('\n':xs) -> parse xs
+    _:xs -> parse []
 
 run :: [BrainFuck] -> IO ()
-run s =   evalStateT ( run' s)  tupElem >> return ()
-    where 
-        lis :: [Int]
-        lis  = replicate 15000 0
-        tupElem :: ([Int],[Int])
-        tupElem = (,) lis lis
+run s =   evalStateT ( run' s) (take 100 [0,0..],take 100 [0,0..])  >> return ()
 
 run' :: [BrainFuck] -> StateT ([Int],[Int]) IO [BrainFuck]
 run' s = case s of
@@ -52,10 +49,10 @@ run' s = case s of
     (ValDec:xs)  -> get >>= (\ ((p:ps),q) -> put ((p-1):ps,q)) >> run' xs
     (PtrInc:xs)  -> get >>= (\ (ps,q:qs) -> put (q:ps,qs)  ) >> run' xs 
     (PtrDec:xs)  -> get >>=  (\ ((p:ps) ,qs) -> put  (ps,p:qs))  >> run' xs 
-    (Put:xs)     -> get >>= ( \ (p:ps,q) -> (liftIO $ putChar $ chr p) ) >> run' xs
+    (Put:xs)     -> get >>= ( \ (p:ps,q) -> (liftIO  $ putChar $ chr p) ) >> run' xs
     (Get:xs)     -> do
         (p:ps,q) <- get
-        t        <- liftIO getLine
+        t        <- liftIO $ hGetLine stdin
         if isNum t
         then do
             put ((read t):ps, q) 
@@ -65,11 +62,8 @@ run' s = case s of
                 isNum :: String -> Bool
                 isNum s = let b = foldl1 (&&) $ map isNumber s
                           in b
-    all@((Loop bf):xs) -> do
-        run' bf 
-        (p,q) <-get 
-        case p of
-          (0:ns) -> run' xs
-          (n:ns) -> run' all 
+    all@((Loop bf):xs) -> run' bf >> get >>= (\(p:_,s) -> case p of
+                                                            0 -> run' xs
+                                                            n -> run' all)
     [] -> return []
 
