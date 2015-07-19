0796c9a92fe42e08662ed76de990cff553902f6e
keizo <keizo.bookman@gmail.com>
2015-07-20 01:09:10 +0900

    Add notification imports

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 module Main where
-import System.Environment
-import System.IO
+import System.Environment (getArgs)
+import System.IO (readFile)
 import Data.Char (chr, ord)
-import Control.Applicative
-import Control.Monad
+import Control.Applicative (many, (<$), (<|>), (<*>), (<*), (*>), (<$>) )
+import Control.Monad (foldM, void)
 import Text.ParserCombinators.ReadP hiding (many)
 
 data BrainFuck = PtrInc | PtrDec | ValInc | ValDec | Put | Get | Loop [BrainFuck]
@@ -47,13 +47,14 @@ adjust f (xs, x, ys) = let y = f x in seq y (xs, y, ys)
 
 run :: [BrainFuck] -> IO ()
 run = void . foldM run' (repeat 0, 0, repeat 0) where
+--    run' :: Tape -> [[BrainFuck]] -> IO Tape
     run' tape@(_, x, _) inst = case inst of
         ValInc -> return $ adjust succ tape
         ValDec -> return $ adjust pred tape
         PtrInc -> return $ next tape
         PtrDec -> return $ prev tape
         Put    -> tape <$ putChar (chr $ (`mod` 128) $ abs x)
-        Get    -> getChar >>= \c -> return $ adjust (const $ ord c) tape
+        Get    -> getChar >>= \ c -> return $ adjust (const $ ord c) tape
         loop@(Loop insts)
             | x == 0 -> return tape
             | otherwise -> foldM run' tape insts >>= flip run' loop
